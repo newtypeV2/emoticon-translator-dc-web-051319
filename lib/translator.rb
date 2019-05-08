cdc900b083d418be08c60a38c3d4984071ac9d7a
Gian Libunao <gianlibunao@gmail.com>
2019-05-08 15:41:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ require "pry"
 def load_library(path)
   # code goes here
   emote = YAML.load_file(path)
-  rHash={"get_meaning": {} , "get_emoticon":{} }
+  rHash={"get_meaning" => {} , "get_emoticon" => {} }
     emote.each{|k,v| 
     rHash[get_meaning]=rHash[:get_meaning].merge(v.last => k)
     rHash[:get_emoticon]=rHash[:get_emoticon].merge(v.first => v.last)
